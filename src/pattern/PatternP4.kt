74521ca1263c58eeff492b8df88c68f5dd9f9a26
vidhimeel <vidhimeel@gmail.com>
2024-08-03 18:14:30 +0530

    PatternP4 Completed.

--- src/pattern/PatternP4.kt
+++ src/pattern/PatternP4.kt
@@ -2,12 +2,28 @@ package pattern
 
 fun main() {
     val rows = 5
+    var k = 0
 
     for (i in rows downTo 1) {
         for (space in 0..rows - i) {
             print("*")
         }
 
-        println("----")
+        print("----")
+
+        while (k != 2 * i) {
+            print("*")
+            ++k
+        }
+
+        for (j in rows downTo  1){
+            print("-")
+        }
+        for (space in 0..rows - i) {
+            print("*")
+        }
+        k = 0
+        println()
+
     }
 }
